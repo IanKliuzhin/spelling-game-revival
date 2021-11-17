b5b182ec88f0633bf2dfbb30e9f7c07cebdced35
Yan Klyuzhin <klyuzhin@uchi.ru>
2021-11-17 14:47:24 +0300

    added setting player type on clicking create game

--- src/pages/MainMenu/MainMenu.tsx
+++ src/pages/MainMenu/MainMenu.tsx
@@ -1,16 +1,19 @@
 import { useStore } from 'src/store';
+import { PlayerType } from 'src/store/gameStore/gameStore';
 import './style.scss';
 
 export const MainMenu = () => {
-  const { pageStore } = useStore();
+  const { gameStore, pageStore } = useStore();
+
+  const handleCreateGameClick = () => {
+    gameStore.setPlayerType(PlayerType.HOST);
+    pageStore.changePage('difficultyChoice');
+  };
 
   return (
     <div className="mainPage">
       <div className="wrapperButton">
-        <div
-          className="button"
-          onClick={() => pageStore.changePage('difficultyChoice')}
-        >
+        <div className="button" onClick={handleCreateGameClick}>
           Создать битву
         </div>
         <div className="button">Присоединиться к битве</div>
