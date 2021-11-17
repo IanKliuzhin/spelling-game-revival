80306075df9317233c6770d5ce1c7916ff5df37a
Yan Klyuzhin <klyuzhin@uchi.ru>
2021-11-17 14:03:37 +0300

    added page store

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { App } from './App';
 import { reportWebVitals } from './reportWebVitals';
 import { ContextStore, createStore } from './store';
 
-const store = createStore({});
+const store = createStore();
 
 ReactDOM.render(
   <ContextStore.Provider value={store}>
