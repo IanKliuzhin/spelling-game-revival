cd914c1d1e4f11747608910682567ea79368bfdf
Yan Klyuzhin <klyuzhin@uchi.ru>
2021-11-17 14:48:18 +0300

    added difficulty choice handling

--- src/pages/DifficultyChoice/DifficultyChoice.tsx
+++ src/pages/DifficultyChoice/DifficultyChoice.tsx
@@ -1,12 +1,27 @@
+import { useStore } from 'src/store';
+import { DifficultyType } from 'src/store/gameStore/gameStore';
 import './style.scss';
 
 export const DifficultyChoice = () => {
+  const { gameStore, pageStore } = useStore();
+
+  const handleDifficultyChoice = (difficulty: DifficultyType) => {
+    gameStore.setDifficulty(difficulty);
+    pageStore.changePage('mainMenu'); // TODO поменять на нужный экран, когда он будет создан
+  };
+
   return (
     <div className="difficultyPage">
       <div className="wrapperButton">
-        <div className="button">easy</div>
-        <div className="button">medium</div>
-        <div className="button">hard</div>
+        {Object.values(DifficultyType).map((difficulty) => (
+          <div
+            className="button"
+            key={difficulty}
+            onClick={() => handleDifficultyChoice(difficulty)}
+          >
+            {difficulty}
+          </div>
+        ))}
       </div>
     </div>
   );
