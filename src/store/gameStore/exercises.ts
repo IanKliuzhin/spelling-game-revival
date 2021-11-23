f91300d6990025afe32115ca78ea4b9368a6ac1d
Yan Klyuzhin <klyuzhin@uchi.ru>
2021-11-23 19:12:28 +0300

    fixed paths

--- src/store/gameStore/exercises.ts
+++ src/store/gameStore/exercises.ts
@@ -6,61 +6,61 @@ export const exercises: Exercises = {
       word: 'ромашка',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_10/2.10_urok_3.4.mp3',
-      imageSrc: '/imagesWords/romashka.jpg',
+      imageSrc: 'imagesWords/romashka.jpg',
     },
     {
       word: 'самолет',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_9/2.9_urok_5.3.mp3',
-      imageSrc: '/imagesWords/samolet.jpg',
+      imageSrc: 'imagesWords/samolet.jpg',
     },
     {
       word: 'апельсин',
       soundSrc:
         ' https://cms-content.uchi.ru/audios/reading/lesson_2_11/2.11_urok_5.2.1.mp3',
-      imageSrc: '/imagesWords/apelsin.jpg',
+      imageSrc: 'imagesWords/apelsin.jpg',
     },
     {
       word: 'акула',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_7/2.7_UROK_1.8.mp3',
-      imageSrc: '/imagesWords/akula.jpg',
+      imageSrc: 'imagesWords/akula.jpg',
     },
     {
       word: 'собака',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_9/2.9_urok_2.6.mp3',
-      imageSrc: '/imagesWords/sobaka.jpg',
+      imageSrc: 'imagesWords/sobaka.jpg',
     },
     {
       word: 'обезьяна',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_11/2.11_urok_1.7.mp3',
-      imageSrc: '/imagesWords/obezana.jpg',
+      imageSrc: 'imagesWords/obezana.jpg',
     },
     {
       word: 'лягушка',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_11/2.11_urok_1.5.mp3',
-      imageSrc: '/imagesWords/lagushka.jpg',
+      imageSrc: 'imagesWords/lagushka.jpg',
     },
     {
       word: 'помидор',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_11/2.11_urok_2.6.mp3',
-      imageSrc: '/imagesWords/pomidor.jpg',
+      imageSrc: 'imagesWords/pomidor.jpg',
     },
     {
       word: 'стрекоза',
       soundSrc:
         ' https://cms-content.uchi.ru/audios/reading/lesson_2_10/2.10_urok_1.2.mp3',
-      imageSrc: '/imagesWords/strekoza.jpg',
+      imageSrc: 'imagesWords/strekoza.jpg',
     },
     {
       word: 'цыпленок',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_10/2.10_urok_1.4.mp3',
-      imageSrc: '/imagesWords/tciplenok.jpg',
+      imageSrc: 'imagesWords/tciplenok.jpg',
     },
   ],
   [DifficultyType.MEDIUM]: [
@@ -68,61 +68,61 @@ export const exercises: Exercises = {
       word: 'гусеница',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_10/2.10_urok_1.6.mp3',
-      imageSrc: '/imagesWords/gusenica.jpg',
+      imageSrc: 'imagesWords/gusenica.jpg',
     },
     {
       word: 'банан',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_10/2.10_urok_2.5.mp3',
-      imageSrc: '/imagesWords/banan.jpg',
+      imageSrc: 'imagesWords/banan.jpg',
     },
     {
       word: 'бочка',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_10/2.10_urok_4.7.mp3',
-      imageSrc: '/imagesWords/bochka.jpg',
+      imageSrc: 'imagesWords/bochka.jpg',
     },
     {
       word: 'корова',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_9/2.9_urok_2.5.mp3',
-      imageSrc: '/imagesWords/corowa.jpg',
+      imageSrc: 'imagesWords/corowa.jpg',
     },
     {
       word: 'лилия',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_10/2.10_urok_3.5.mp3',
-      imageSrc: '/imagesWords/lilia.jpg',
+      imageSrc: 'imagesWords/lilia.jpg',
     },
     {
       word: 'баклажан',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_11/2.11_urok_5.4.mp3',
-      imageSrc: '/imagesWords/baklajan.jpg',
+      imageSrc: 'imagesWords/baklajan.jpg',
     },
     {
       word: 'ракета',
       soundSrc:
         ' https://cms-content.uchi.ru/audios/reading/lesson_2_9/2.9_urok_5.9.mp3',
-      imageSrc: '/imagesWords/raketa.jpg',
+      imageSrc: 'imagesWords/raketa.jpg',
     },
     {
       word: 'дельфин',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_1_9/D/1.9_urok_d6.7.mp3',
-      imageSrc: '/imagesWords/delfin.jpg',
+      imageSrc: 'imagesWords/delfin.jpg',
     },
     {
       word: 'зебра',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_1_13/1.13_urok_%20YUE2.4.mp3',
-      imageSrc: '/imagesWords/zebra.jpg',
+      imageSrc: 'imagesWords/zebra.jpg',
     },
     {
       word: 'фонарик',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_11/2.11_urok_3.5.mp3',
-      imageSrc: '/imagesWords/fonarik.jpg',
+      imageSrc: 'imagesWords/fonarik.jpg',
     },
   ],
   [DifficultyType.HARD]: [
@@ -130,61 +130,61 @@ export const exercises: Exercises = {
       word: 'пицца',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_1_10/1.10_urok_z6.7.mp3',
-      imageSrc: '/imagesWords/picca.jpg',
+      imageSrc: 'imagesWords/picca.jpg',
     },
     {
       word: 'слон',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_1_10/1.10_ZS_1.2.mp3',
-      imageSrc: '/imagesWords/slon.jpg',
+      imageSrc: 'imagesWords/slon.jpg',
     },
     {
       word: 'зонт',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_1_10/1.10_ZS_1.4.mp3',
-      imageSrc: '/imagesWords/zont.jpg',
+      imageSrc: 'imagesWords/zont.jpg',
     },
     {
       word: 'лодка',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_8/2.8_urok_1.6.mp3',
-      imageSrc: '/imagesWords/lodka.jpg',
+      imageSrc: 'imagesWords/lodka.jpg',
     },
     {
       word: 'картофель',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_11/2.11_urok_5.3.1.mp3',
-      imageSrc: '/imagesWords/kartofel.jpg',
+      imageSrc: 'imagesWords/kartofel.jpg',
     },
     {
       word: 'лимон',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_7/2.7_UROK_3.10.mp3',
-      imageSrc: '/imagesWords/limon.jpeg',
+      imageSrc: 'imagesWords/limon.jpeg',
     },
     {
       word: 'кальмар',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_7/2.7_UROK_5.2.mp3',
-      imageSrc: '/imagesWords/kalmar.jpg',
+      imageSrc: 'imagesWords/kalmar.jpg',
     },
     {
       word: 'очки',
       soundSrc:
         ' https://cms-content.uchi.ru/audios/reading/lesson_2_6/2.6_urok3.6.mp3',
-      imageSrc: '/imagesWords/ochki.jpg',
+      imageSrc: 'imagesWords/ochki.jpg',
     },
     {
       word: 'туфли',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_2_6/2.6_urok5.7.mp3',
-      imageSrc: '/imagesWords/tufli.jpg',
+      imageSrc: 'imagesWords/tufli.jpg',
     },
     {
       word: 'дятел',
       soundSrc:
         'https://cms-content.uchi.ru/audios/reading/lesson_1_9/D/1.9_urok_d6.6.mp3',
-      imageSrc: '/imagesWords/datel.jpg',
+      imageSrc: 'imagesWords/datel.jpg',
     },
   ],
 };
