83c1614a805eb35f4004f13d76806917ef586354
Yan Klyuzhin <klyuzhin@uchi.ru>
2022-02-09 18:40:05 +0300

    updated firebase config to use another realtime db

--- src/store/connectionStore/firebaseConfig.ts
+++ src/store/connectionStore/firebaseConfig.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCfIbQmfN8ZWunC13zhJyDoQ1aL34mKvHI',
-  authDomain: 'react-p2p-test.firebaseapp.com',
+  apiKey: 'AIzaSyChCSsHJJxilbaqNx9w1tPKNbrv7z2Nj9E',
+  authDomain: 'react-p2p-test-retry.firebaseapp.com',
   databaseURL:
-    'https://react-p2p-test-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'react-p2p-test',
-  storageBucket: 'react-p2p-test.appspot.com',
-  messagingSenderId: '54650620648',
-  appId: '1:54650620648:web:a3a8bc21fd8bcd4782975d',
+    'https://react-p2p-test-retry-default-rtdb.europe-west1.firebasedatabase.app/',
+  projectId: 'react-p2p-test-retry',
+  storageBucket: 'react-p2p-test-retry.appspot.com',
+  messagingSenderId: '222327022151',
+  appId: '1:222327022151:web:5496f8cb285955c68a4762',
 };
